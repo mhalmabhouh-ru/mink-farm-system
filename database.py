83c6aa343ce45ca011acfe9194b08417e93ac50d
mhalmabhouh-ru <mh.almabhouh@std.alaqsa.edu.ps>
2026-04-14 12:40:58 +0300

    fix DB02

--- database.py
+++ database.py
@@ -2,21 +2,17 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# 🔗 جلب رابط قاعدة البيانات من البيئة (Railway / Server)
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# 🛠️ إنشاء الاتصال بالداتابيز
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
 )
 
-# 🧠 جلسة التعامل مع الداتابيز
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-# 🧱 قاعدة الموديلات
 Base = declarative_base()
