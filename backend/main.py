82c654024e74bfc89b8cbad5bee68d82ebefb24e
mhalmabhouh-ru <mh.almabhouh@std.alaqsa.edu.ps>
2026-04-14 15:45:50 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
 
 Base.metadata.create_all(bind=engine)
 
-BASE_DIR = Path(file).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent
 FRONTEND_DIR = BASE_DIR / "frontend"
 
 if FRONTEND_DIR.exists():
