79a5c865b5f1066810663b9290cda62697f83157
mhalmabhouh-ru <mh.almabhouh@std.alaqsa.edu.ps>
2026-04-14 12:36:50 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,15 @@
-
-from sqlalchemy import Column, Integer, String 
+from sqlalchemy import Column, Integer, String
 from database import Base
-class Mink(Base): __tablename__ = "minks"
-id = Column(Integer, primary_key=True, index=True) 
-female_no = Column(String, index=True) 
-year = Column(Integer) 
-quality = Column(String) 
-last_year_kids = Column(Integer) 
-this_year_kids = Column(Integer) 
-shed = Column(String) 
-colour = Column(String)
+
+class Mink(Base):
+    tablename = "minks"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    female_no = Column(String, index=True)
+    year = Column(Integer)
+    quality = Column(String)
+    last_year_kids = Column(Integer)
+    this_year_kids = Column(Integer)
+    shed = Column(String)
+    colour = Column(String)
